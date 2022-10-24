7abcee3fb63302315163ddf7d86a95643d441c87
Zac Lip <zaclipperd@gmail.com>
2022-10-24 00:01:25 -0500

    added game playing and prints, ISSUES STILL minimax is choosing wrong heuristic value i think, also crash when board is getting full, will never completly fill for a tie

--- HW4/main.py
+++ HW4/main.py
@@ -1,6 +1,10 @@
 #imports
 import copy
+from time import process_time
 
+#global variables
+nodes_created = 0
+total_nodes_created = 0
 
 # board class
 class Board:
@@ -30,14 +34,12 @@ class Board:
         self.turn_number = 1
         # make the first move as specified in the project document
         # project doc is indexed from 1, while we are indexing at 0 so subtract one
-        self.make_move(3-1, 4-1)
-        self.make_move(3-1, 3-1)
 
     # make move
     # has the current player make a move. Mark the requested space and then swap player
     def make_move(self, row, column):
-        #self.print_board()
-        # print("*Player " + str(self.current_player) + " is making the move [" + str(row+1) + "," + str(column+1) + "]")
+        # self.print_board()
+        # print("*Player " + str(self.current_player) + "is making the move [" + str(row+1) + "," + str(column+1) + "]")
         self.board[row][column] = self.current_player
         self.swap_player_turn()
         self.turn_number += 1
@@ -87,6 +89,7 @@ class Board:
             # print row header
             print(f"| {c} ", end='')
             for space in row:
+                pass
                 print(f"| {space} ", end='')
             print(f"| ")
             c += 1
@@ -155,39 +158,38 @@ class Board:
         return list_moves
 
     def initMinMax(self, max_depth, depth, active_turn):
-      best_move = "-1,-1"
-      score = -100000
-      depth += 1
-
-      current_player = self.current_player
-
-      # populate score
-      for m in self.available_moves():
-          # print(" --------- " + m.split(",")[1])
-          # create possible game state``
-          possible_board = copy.deepcopy(self)
-          # make m move on possible game state
-          possible_board.make_move(int(m.split(",")[0]), int(m.split(",")[1]))
-          passed_turn = possible_board.current_player
-          if possible_board.current_player == 'X':
-              possible_board.current_player = 'O'
-          else:
-              possible_board.current_player = 'X'
-
-          if max_depth >= depth:
-            candidate_score = possible_board.minmax(max_depth, depth, passed_turn)
+        best_move = "-1,-1"
+        score = -100000
+        depth += 1
+        current_player = self.current_player
+        # populate score
+        for m in self.available_moves():
+            # print(" --------- " + m.split(",")[1])
+            # create possible game state``
+            possible_board = copy.deepcopy(self)
+            # make m move on possible game state
+            possible_board.make_move(int(m.split(",")[0]), int(m.split(",")[1]))
+            passed_turn = possible_board.current_player
+            if possible_board.current_player == 'X':
+                possible_board.current_player = 'O'
+            else:
+                possible_board.current_player = 'X'
+            if max_depth >= depth:
+                candidate_score = possible_board.minmax(max_depth, depth, passed_turn)
             # print(score)
             if score < candidate_score:
-              score = candidate_score
-              # print(score)
-              best_move = m
-      # print(score)
-      # print("best move: ", best_move)
-      # print()
-      return best_move
+                score = candidate_score
+                # print(score)
+                best_move = m
+        # print(score)
+        # print("best move: ", best_move)
+        # print()
+        return best_move
               
 
     def minmax(self, max_depth, depth, active_turn):
+        global nodes_created
+        nodes_created += 1
         moves = []
         scores = []
         depth += 1
@@ -210,8 +212,9 @@ class Board:
                 possible_board = copy.deepcopy(self)
                 # make m move on possible game state
                 possible_board.make_move(int(m.split(",")[0]), int(m.split(",")[1]))
-                # print("we are adding to heuristic values here\n adding : " + str(evaluate_state(possible_board, active_turn))
-                #         + "\n recursive depth iss: " + str(depth) + "Nodes generated: " + str(possible_board.node_number))
+                # print("we are adding to heuristic values here\n adding : "
+                # + str(evaluate_state(possible_board, active_turn))
+                # + "\n recursive depth iss: " + str(depth) + "Nodes generated: " + str(possible_board.node_number))
                 scores.append(possible_board.minmax(max_depth, depth, passed_turn))
             # get score of possible game state and push it to move
         else:
@@ -221,7 +224,7 @@ class Board:
         # print("list of heurisic values calculated TTTT: ", scores,  "\n recursive depth iss: " + str(depth))
         # push move to move
         if active_turn == passed_turn:
-        # max score
+            # max score
             # heuristic value has all values of possible board states of all possible moves`
             return max(scores)
         else:
@@ -250,7 +253,8 @@ def evaluate_state(board_state, current_player):
     for row in board_state.board:
         for space in row:
             if space != '-':
-                # print("================================\nrunning checks on [" + str(row_number+1) + "," + str(column_number+1) + "]")
+                # print("================================\n running checks on ["
+                # + str(row_number+1) + "," + str(column_number+1) + "]")
                 # check horizontals
                 # print("------------------------\nStart of horizontal")
                 state_value += check_horizontal(board_state, row_number, column_number, current_player)
@@ -281,8 +285,8 @@ def evaluate_state(board_state, current_player):
 # determine heuristic value of current row u
 # used by check_horizontals check_verticals check_diagonals in order to avoid repeating it each time
 def check_return_heuristic( number_in_row, open_status, same_player_status):
-    #print("Values we are getting in check return are:\nnumber_in_rows: " + str(number_in_row) + "\nopen_status: "
-          #+ str(open_status) + "\nsame_player_status: "+str(same_player_status))
+    # print("Values we are getting in check return are:\nnumber_in_rows: " + str(number_in_row) + "\nopen_status: "
+    # + str(open_status) + "\n same_player_status: "+str(same_player_status))
     # if we have 4 in a row then this is a winning state, and we return 1000 to indicate this
     # (-1000 if winning state for opponent)
     if number_in_row == 4:
@@ -362,7 +366,7 @@ def check_verticals(board_state, row, column, current_player):
     # if row is zero and nothing is in front of it
     if row > 0 and board_state.board[row-1][column] == '-':
         open_status += 1
-    #print("open status at start is: " + str(open_status))
+    # print("open status at start is: " + str(open_status))
     # if space before is same as current space, then we have already accounted for it being in a row
     # and do not need to run this function again (check to make sure we arent checking out of bounds)
     if row > 0 and board_state.board[row-1][column] == board_state.board[row][column]:
@@ -372,20 +376,20 @@ def check_verticals(board_state, row, column, current_player):
     while True:
         # if we reach the end of possible members of this row (out for board bounds)
         if row+number_in_row > board_state.number_of_rows-1:
-            #print("we are entering out of bounds break")
+            # print("we are entering out of bounds break")
             break
         # if the next in row is same as current space then we have row of that value
         elif board_state.board[row][column] == board_state.board[row+number_in_row][column]:
-            #print("we are entering row is good break")
+            # print("we are entering row is good break")
             number_in_row += 1
         # if next space is open indicate that and break while as there is no more row
         elif board_state.board[row+number_in_row][column] == '-':
-            #print("we are entering end space is break")
+            # print("we are entering end space is break")
             open_status += 1
             break
         # only other condition is that the next space is other value so row is broken
         else:
-            #print("we are entering row broken break")
+            # print("we are entering row broken break")
             break
     # need space value and the current player to finish evaluating, 0 means same as player, 1 is different
     same_player_status=0
@@ -403,35 +407,35 @@ def check_diagonals_right(board_state, row, column, current_player):
     if row > 0 and column > 0:
         if board_state.board[row-1][column-1] == '-':
             open_status += 1
-    #print("open status at start is: " + str(open_status))
+    # print("open status at start is: " + str(open_status))
     # if space before is same as current space, then we have already accounted for it being in a row
     # and do not need to run this function again (check to make sure we are not checking out of bounds)
-    #print(board_state.board[row - 1][column - 1])
-    #print(board_state.board[row][column])
-    #print(board_state.board[row - 1][column - 1] == board_state.board[row][column])
+    # print(board_state.board[row - 1][column - 1])
+    # print(board_state.board[row][column])
+    # print(board_state.board[row - 1][column - 1] == board_state.board[row][column])
     if row > 0 and column > 0:
         if board_state.board[row - 1][column - 1] == board_state.board[row][column]:
-            #print("we are entering been there done that")
+            # print("we are entering been there done that")
             return 0
     number_in_row = 1
     # count number of same in a row
     while True:
         # if we reach the end of possible members of this row (out for board bounds)
         if row + number_in_row > board_state.number_of_rows-1 or column + number_in_row > board_state.number_of_columns-1:
-            #print("we are entering out of bounds break")
+            # print("we are entering out of bounds break")
             break
         # if the next in row is same as current space then we have row of that value
         elif board_state.board[row][column] == board_state.board[row + number_in_row][column + number_in_row]:
-            #print("we are entering row is good break")
+            # print("we are entering row is good break")
             number_in_row += 1
         # if next space is open indicate that and break while as there is no more row
         elif board_state.board[row + number_in_row][column + number_in_row] == '-':
-            #print("we are entering end space is break")
+            # print("we are entering end space is break")
             open_status += 1
             break
         # only other condition is that the next space is other value so row is broken
         else:
-            #print("we are entering row broken break")
+            # print("we are entering row broken break")
             break
     # need space value and the current player to finish evaluating, 0 means same as player, 1 is different
     same_player_status = 0
@@ -449,32 +453,32 @@ def check_diagonals_left(board_state, row, column, current_player):
     if row > 0 and column < board_state.number_of_columns-1:
         if board_state.board[row-1][column+1] == '-':
             open_status += 1
-    #print("open status at start is: " + str(open_status))
+    # print("open status at start is: " + str(open_status))
     # if space before is same as current space, then we have already accounted for it being in a row
     # and do not need to run this function again (check to make sure we are not checking out of bounds)
     if row > 0 and column < board_state.number_of_columns-1:
         if board_state.board[row - 1][column + 1] == board_state.board[row][column]:
-            #print("we are entering been there done that")
+            # print("we are entering been there done that")
             return 0
     number_in_row = 1
     # count number of same in a row
     while True:
         # if we reach the end of possible members of this row (out for board bounds)
         if row + number_in_row > board_state.number_of_rows-1 or column - number_in_row < 0:
-            #print("we are entering out of bounds break")
+            # print("we are entering out of bounds break")
             break
         # if the next in row is same as current space then we have row of that value
         elif board_state.board[row][column] == board_state.board[row + number_in_row][column - number_in_row]:
-            #print("we are entering row is good break")
+            # print("we are entering row is good break")
             number_in_row += 1
         # if next space is open indicate that and break while as there is no more row
         elif board_state.board[row + number_in_row][column - number_in_row] == '-':
-            #print("we are entering end space is break")
+            # print("we are entering end space is break")
             open_status += 1
             break
         # only other condition is that the next space is other value so row is broken
         else:
-            #print("we are entering row broken break")
+            # print("we are entering row broken break")
             break
     # need space value and the current player to finish evaluating, 0 means same as player, 1 is different
     same_player_status = 0
@@ -483,90 +487,89 @@ def check_diagonals_left(board_state, row, column, current_player):
     # evaluate the row value and return it
     return check_return_heuristic(number_in_row, open_status, same_player_status)
 
+
+def play_a_game(board):
+    # start game timer
+    t1_start = process_time()
+    # import some globals
+    global nodes_created
+    global total_nodes_created
+    i = 0
+    winner = 'It is a Tie'
+    while not board.is_board_filled():
+        board.print_board()
+        max_depth = 0
+        if board.current_player == 'X':
+            # print(evaluate_state(board, 'X'))
+            if evaluate_state(board, 'X') > 700:
+                winner = 'Player X'
+                break
+            elif evaluate_state(board, 'X') < -700:
+                winner = 'Player O'
+                break
+            max_depth = 2
+        else:
+            # print(evaluate_state(board, 'O'))
+            if evaluate_state(board, 'O') > 700:
+                winner = 'Player O'
+                break
+            elif evaluate_state(board, 'O') < -700:
+                winner = 'Player X'
+                break
+            max_depth = 4
+        # determine best move using minimax
+        t2_start = process_time()
+        best_move = board.initMinMax(max_depth, 0, board.current_player)
+        t2_stop = process_time()
+        print("* Player " + str(board.current_player) + " is making the move [" + str(int(best_move.split(",")[0]))
+              + "," + str(best_move.split(",")[1]) + "]\n*    Player " + str(board.current_player)
+              + " is running minimax algorithm on a " + str(max_depth)
+              + "-ply game tree\n*    total nodes created by minimax for this move: "
+              + str(nodes_created) + "\n*    Elapse time: " + str(t2_stop - t2_start) + " seconds")
+        # update node tracking globals
+        total_nodes_created += nodes_created
+        nodes_created = 0
+        board.make_move(int(best_move.split(",")[0]), int(best_move.split(",")[1]))
+    # stop game timer
+    t1_stop = process_time()
+    print("\n\n****************************\n  !!!!!!GAME IS OVER!!!!!!"
+          "\n****************************\nThe winner of the game is:   "
+          + winner + "\nTotal nodes created for minmax: " + str(total_nodes_created)
+          + "\nTotal runtime of game: ", str(t1_stop - t1_start) + " seconds")
+    # if someone wins
+
+
 # main
 def main():
+
+    # Create Board
     game_board = Board()
-    # test state from project file
-    game_board.current_player ='O'
-    game_board.make_move(1, 2)
-    game_board.make_move(1, 3)
-    game_board.make_move(2, 2)
-    game_board.make_move(3, 2)
-    game_board.make_move(3, 1)
-    game_board.make_move(3, 3)
-    game_board.make_move(3, 4)
-    game_board.make_move(4, 2)
-    game_board.current_player = 'X'
-    game_board.make_move(2, 4)
-    game_board.current_player = 'O'
-    game_board.make_move(2, 3)
-    game_board.current_player = 'X'
-    # game_board.make_move(1, 5)
-    # game_board.current_player = 'X'
-    game_board.print_board()
 
-    # print(game_board.board)
+    # SET UP STATES
+    game_board.make_move(3 - 1, 4 - 1)
+    game_board.make_move(3 - 1, 3 - 1)
+    game_board.current_player = 'X'
+    # ASIIGNMENT STATE
 
-    # game_board.board=[['X', '-', 'O', 'O', 'O', 'X'], ['-', '-', '-', 'X', 'X', 'O'], ['-', '-', 'O', 'X', '-', '-'], ['-', '-', 'O', 'X', '-', '-'], ['O', '-', '-', '-', '-', 'X']]
-    # game_board.print_board()
-    # value_of_state = evaluate_state(game_board)
-    # print(game_board.current_player)
-    # print(" \n\n" + str(value_of_state))
-    # game_board.make_move(1, 1)
+    # EXAMPLE #! (state from project file)
     # game_board.current_player = 'O'
-    #
-    # game_board.print_board()
-    # value_of_state = evaluate_state(game_board)
-    # print(game_board.current_player)
-    # print(" \n\n" + str(value_of_state))
+    # game_board.make_move(1, 2)
+    # game_board.make_move(1, 3)
+    # game_board.make_move(2, 2)
+    # game_board.make_move(3, 2)
+    # game_board.make_move(3, 1)
+    # game_board.make_move(3, 3)
+    # game_board.make_move(3, 4)
+    # game_board.make_move(4, 2)
+    # game_board.make_move(2, 3)
+    # game_board.make_move(2, 4)
+    # game_board.current_player = 'X'
 
-    x = game_board.initMinMax(2, 0, game_board.current_player)
-    print(x)
-    print(game_board.print_board())
-    game_board.make_move(int(x.split(",")[0]), int(x.split(",")[1]))
-    print(game_board.print_board())
+    # EXAMPLE #2
 
-    # x = game_board.initMinMax(4, 0, game_board.current_player)
-    # print(x)
-    # print(game_board.print_board())
-    # game_board.make_move(int(x.split(",")[0]), int(x.split(",")[1]))
-    # print(game_board.print_board())
-    #
-    # x = game_board.initMinMax(2, 0, game_board.current_player)
-    # print(x)
-    # print(game_board.print_board())
-    # game_board.make_move(int(x.split(",")[0]), int(x.split(",")[1]))
-    # print(game_board.print_board())
-    #
-    # x = game_board.initMinMax(4, 0, game_board.current_player)
-    # print(x)
-    # print(game_board.print_board())
-    # game_board.make_move(int(x.split(",")[0]), int(x.split(",")[1]))
-    # print(game_board.print_board())
-    #
-    # x = game_board.initMinMax(2, 0, game_board.current_player)
-    # print(x)
-    # print(game_board.print_board())
-    # game_board.make_move(int(x.split(",")[0]), int(x.split(",")[1]))
-    # print(game_board.print_board())
-    #
-    # x = game_board.initMinMax(4, 0, game_board.current_player)
-    # print(x)
-    # print(game_board.print_board())
-    # game_board.make_move(int(x.split(",")[0]), int(x.split(",")[1]))
-    # print(game_board.print_board())
-    #
-    # x = game_board.initMinMax(2, 0, game_board.current_player)
-    # print(x)
-    # print(game_board.print_board())
-    # game_board.make_move(int(x.split(",")[0]), int(x.split(",")[1]))
-    # print(game_board.print_board())
-    #
-    # x = game_board.initMinMax(4, 0, game_board.current_player)
-    # print(x)
-    # print(game_board.print_board())
-    # game_board.make_move(int(x.split(",")[0]), int(x.split(",")[1]))
-    # print(game_board.print_board())
+    # Play a Game
+    play_a_game(game_board)
+
 
 
 if __name__ == "__main__":
